f60785535a7452da2ba02055d5da0ae1a8be40f3
Zhakhongir <zhakhongir.sadyrov>
2022-08-30 23:32:24 +0600

    first commit

--- internal/book/controllers/handlers.go
+++ internal/book/controllers/handlers.go
@@ -12,9 +12,30 @@ import (
 
 func RouterCreation() *httprouter.Router {
 	router := httprouter.New()
-	router.GET("/:id", findBookById)
-	router.POST("/books", insertBook)
+	router.GET("/api/books/:id", findBookById)
+	router.GET("/api/books", getAll)
+	router.POST("/api/books", insertBook)
+	router.PUT("/api/books", updateBook)
 	return router
+}
+
+func getAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	db := database.Init()
+	writer.Header().Set("Content-Type", "application/json")
+	books := database.GetAllBooks(db)
+	json.NewEncoder(writer).Encode(books)
+}
+
+func updateBook(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	db := database.Init()
+	writer.Header().Set("Content-Type", "application/json")
+	var b book.Book
+	_ = json.NewDecoder(request.Body).Decode(&b)
+	database.UpdateBook(b, db)
+	err := db.Close()
+	if err != nil {
+		log.Println("Closing connection error")
+	}
 }
 
 func insertBook(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
--- internal/database/queries.go
+++ internal/database/queries.go
@@ -3,12 +3,17 @@ package database
 import (
 	"booksCRUD/internal/book"
 	"database/sql"
+	"fmt"
 	"log"
 )
 
+const insertStatement string = "insert into books (name, price) values ($1, $2)"
+const findByIdStatement string = "select * from books where id=$1"
+const updateStatement string = "update books set name = $1, price = $2 where id = $3"
+const getAllStatement string = "select * from books"
+
 func Create(book book.Book, db *sql.DB) {
-	sqlStatement := "insert into books (name, price) values ($1, $2)"
-	_, err := db.Exec(sqlStatement, book.Name, book.Price)
+	_, err := db.Exec(insertStatement, book.Name, book.Price)
 	if err != nil {
 		panic(err)
 	}
@@ -16,7 +21,6 @@ func Create(book book.Book, db *sql.DB) {
 }
 
 func FindBookById(id int, db *sql.DB) book.Book {
-	findByIdStatement := "select * from books where id=$1"
 	row := db.QueryRow(findByIdStatement, id)
 	b := book.Book{}
 	err := row.Scan(&b.ID, &b.Name, &b.Price)
@@ -25,4 +29,29 @@ func FindBookById(id int, db *sql.DB) book.Book {
 		return b
 	}
 	return b
+}
+
+func UpdateBook(book book.Book, db *sql.DB) {
+	_, err := db.Exec(updateStatement, book.Name, book.Price, book.ID)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetAllBooks(db *sql.DB) []book.Book {
+	books := []book.Book{}
+	rows, err := db.Query(getAllStatement)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		b := book.Book{}
+		err := rows.Scan(&b.ID, &b.Name, &b.Price)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		books = append(books, b)
+	}
+	return books
 }
